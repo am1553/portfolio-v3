174889bf080ffcafa1357fe650ac46172bb2e2fa
Aryan Mehta <aryanmehta@Aryans-MacBook-Air.local>
2022-01-10 22:10:23 +0000

    completed portfolio

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const links = document.querySelectorAll('.links');
 // EVENTLISTENERS
 
 navBtn.addEventListener('click', navBtnClick);
+
 for(let i = 0; i < links.length; i++) {
     links[i].addEventListener('click', closePanel)
 }
